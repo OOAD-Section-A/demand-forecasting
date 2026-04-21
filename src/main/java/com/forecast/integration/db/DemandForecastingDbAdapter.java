aef83e92422b7f878073f886ed021a4e7851eb22
LilRag <anurags7475@gmail.com>
2026-04-21 07:38:23 +0530

    final working update

--- src/main/java/com/forecast/integration/db/DemandForecastingDbAdapter.java
+++ src/main/java/com/forecast/integration/db/DemandForecastingDbAdapter.java
@@ -22,36 +22,72 @@ public class DemandForecastingDbAdapter {
         adapter.createForecast(forecast);
     }
 
+    public void deleteForecast(String forecastId) {
+        adapter.deleteForecast(forecastId);
+    }
+
     public void createSalesRecord(SalesRecord salesRecord) {
         adapter.createSalesRecord(salesRecord);
+    }
+
+    public void deleteSalesRecord(String saleId) {
+        adapter.deleteSalesRecord(saleId);
     }
 
     public void createHolidayCalendar(HolidayCalendar holidayCalendar) {
         adapter.createHolidayCalendar(holidayCalendar);
     }
 
+    public void deleteHolidayCalendar(String holidayId) {
+        adapter.deleteHolidayCalendar(holidayId);
+    }
+
     public void createPromotionalCalendar(PromotionalCalendar promotionalCalendar) {
         adapter.createPromotionalCalendar(promotionalCalendar);
+    }
+
+    public void deletePromotionalCalendar(String promoCalendarId) {
+        adapter.deletePromotionalCalendar(promoCalendarId);
     }
 
     public void createProductMetadata(ProductMetadata productMetadata) {
         adapter.createProductMetadata(productMetadata);
     }
 
+    public void deleteProductMetadata(String productId) {
+        adapter.deleteProductMetadata(productId);
+    }
+
     public void createProductLifecycleStage(ProductLifecycleStage stage) {
         adapter.createProductLifecycleStage(stage);
+    }
+
+    public void deleteProductLifecycleStage(String lifecycleId) {
+        adapter.deleteProductLifecycleStage(lifecycleId);
     }
 
     public void createInventorySupply(InventorySupply inventorySupply) {
         adapter.createInventorySupply(inventorySupply);
     }
 
+    public void deleteInventorySupply(String productId) {
+        adapter.deleteInventorySupply(productId);
+    }
+
     public void createForecastPerformanceMetric(ForecastPerformanceMetric metric) {
         adapter.createForecastPerformanceMetric(metric);
     }
 
+    public void deleteForecastPerformanceMetric(String evalId) {
+        adapter.deleteForecastPerformanceMetric(evalId);
+    }
+
     public void createForecastTimeseries(ForecastTimeseries ts) {
         adapter.createForecastTimeseries(ts);
+    }
+
+    public void deleteForecastTimeseries(String timeseriesId) {
+        adapter.deleteForecastTimeseries(timeseriesId);
     }
 
     public void createBatchForecastTimeseries(List<ForecastTimeseries> list) {
